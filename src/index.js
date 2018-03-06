c8449970da697691d3164d050e24d14477b036ff
brighthero <matthias.hamborg@gmail.com>
2018-03-06 15:20:04 +0100

    add Redux Debug

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+require('react-devtools-core').connectToDevTools({host: '169.254.187.227'})
+
 import React from "react";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from "./reducers";
 import App from "./containers/App";
 
 const middleware = [thunk];
 
-const store = createStore(reducer, applyMiddleware(...middleware));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default (ProviderApp = () => {
   return (
