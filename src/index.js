01ea878f9a365127899bd5e63c91521068c3191d
brighthero <matthias.hamborg@gmail.com>
2018-03-06 12:39:30 +0100

    Reorder

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-import App from "./containers/App";
-
 import reducer from "./reducers";
+import App from "./containers/App";
 
 const middleware = [thunk];
 
