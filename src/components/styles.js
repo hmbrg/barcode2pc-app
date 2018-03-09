8760b3da8954ba144e16d6a14ca7ac41bfed05f5
brighthero <matthias.hamborg@gmail.com>
2018-03-10 00:43:47 +0100

    Colors

---
+++ src/components/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+
+// https://coolors.co/f9c80e-e84855-0075f2-47ffa7-46237a
+export const colors = {
+	yellow: "#F9C80E",
+	red: "#E84855",
+	blue: "#0075F2",
+	green: "#47FFA7"
+};
